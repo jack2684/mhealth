1ee56d680ad800fabab3c9751dc51954427bd1b9
Junjie Guan <gjj2684@gmail.com>
2015-02-22 00:04:14 -0500

    Building a Simple User Interface

--- visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/MainActivity.java
+++ visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.jack.brainwaves;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
