eca123074de00f66509cd950823104a89c6c29f0
Junjie Guan <gjj2684@gmail.com>
2015-03-01 12:03:25 -0500

    Add splash screen, and modify the pie style

--- visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/SplashScreenActivity.java
+++ visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/SplashScreenActivity.java
@@ -1,19 +1,62 @@
 package com.example.jack.brainwaves;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class SplashScreenActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
 
+//        // Get screen size
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        int width=dm.widthPixels;
+//        final int height=dm.heightPixels;
+//        int dens=dm.densityDpi;
+//
+//        // Set logo and text position
+//        RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT);
+//        p.addRule(RelativeLayout.BELOW, R.id.splashlogo);
+//        TextView splashtext = (TextView) findViewById(R.id.splashtext);
+//        splashtext.setLayoutParams(p);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
