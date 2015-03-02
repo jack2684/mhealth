359f9b36f0ccb610bc026409113b5b09a3c1b138
Junjie Guan <gjj2684@gmail.com>
2015-03-01 22:09:58 -0500

    A prototype of runnable swipe pagers

--- visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/MainActivity.java
+++ visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/MainActivity.java
@@ -1,277 +1,142 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.jack.brainwaves;
 
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.graphics.Color;
+import com.example.jack.brainwaves.Cheeses;
+import com.example.jack.brainwaves.R;
+
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.SeekBar;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.androidplot.pie.PieChart;
-import com.androidplot.pie.PieRenderer;
-import com.androidplot.pie.Segment;
-import com.androidplot.pie.SegmentFormatter;
-
-import java.util.Random;
-
+//BEGIN_INCLUDE(complete)
 public class MainActivity extends Activity {
+    static final int NUM_ITEMS = 10;
 
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    MyAdapter mAdapter;
 
-    private boolean firstTime;
-
-    private TextView stressScoreTextView;
-    private TextView durationTextView;
-    private PieChart pie;
-    private static final int[] buttonids = {
-            R.id.startLogXYPlotButton,
-            R.id.startOrSensorExButton,
-            R.id.museandpssbtn,
-    };
-
-    private Segment s1;
-    private Segment s2;
-    SegmentFormatter sf1 = new SegmentFormatter();
-    SegmentFormatter sf2 = new SegmentFormatter();
-
-    private float goldenPercentageOutput = .83f;
-    private float dynamicPercentage;
-
-    private Thread myThread;
-
-    private DynamicScaleShow data;
-
-    int dynamicColorR = 255;
-    int dynamicColorG = 125;
-    int dynamicColorB = 20;
+    ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        Button startLogXYPlotButton = (Button)findViewById(R.id.startLogXYPlotButton);
-        startLogXYPlotButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LogXYPlotActivity.class));
+        mAdapter = new MyAdapter(getFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
+        // Watch for button clicks.
+        Button button = (Button)findViewById(R.id.goto_first);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mPager.setCurrentItem(0);
             }
         });
-
-        Button startOrSensorExButton = (Button) findViewById(R.id.startOrSensorExButton);
-        startOrSensorExButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RealtimePlotActivity.class));
-            }
-        });
-
-        Button startMuseButton = (Button) findViewById(R.id.museandpssbtn);
-        startMuseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MuseActivity.class));
-            }
-        });
-
-        SeekBar durationBar = (SeekBar) findViewById(R.id.durationSeekBar);
-        durationBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-                redrawData();
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
-                // TODO Auto-generated method stub
-                translateProgress2Duration(seekBar);
-            }
-        });
-
-        TextView startRefeshAnimation = (TextView) findViewById(R.id.donutSizeTextView);
-        startRefeshAnimation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                redrawData();
-            }
-        });
-
-        // initialize Views:
-        pie = (PieChart) findViewById(R.id.mySimplePieChart);
-        stressScoreTextView = (TextView) findViewById(R.id.donutSizeTextView);
-        durationTextView = (TextView) findViewById(R.id.durationTextView);
-        updateDonutText();
-        updateDonutColor();
-
-        s1 = new Segment("", 20);
-        s2 = new Segment("", 50);
-
-        sf1.configure(getApplicationContext(), R.xml.pie_segment_formatter1);
-        sf1.getFillPaint().setColor(getDynamicColor());
-        sf2.configure(getApplicationContext(), R.xml.pie_segment_formatter2);
-
-        pie.setPlotMarginBottom(0);
-        pie.addSegment(s1, sf1);
-        pie.addSegment(s2, sf2);
-        pie.getBorderPaint().setColor(Color.TRANSPARENT);
-        pie.getBackgroundPaint().setColor(Color.TRANSPARENT);
-        pie.getRenderer(PieRenderer.class).setDonutSize(.85f, PieRenderer.DonutMode.PERCENT);
-        pie.redraw();
-
-        data = new DynamicScaleShow();
-        firstTime = true;
-    }
-
-    @Override
-    public void onResume() {
-        // kick off the data generating thread:
-        if(firstTime) {
-            redrawData();
-            firstTime = false;
-        }
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        data.stopThread();
-        super.onPause();
-    }
-
-    class DynamicScaleShow extends Activity implements Runnable {
-
-        private boolean keepRunning = false;
-
-        public void stopThread() {
-            keepRunning = false;
-        }
-
-        //@Override
-        public void run() {
-            try {
-                keepRunning = true;
-                s1.setValue(0);
-                dynamicPercentage = .0f;
-//                float diff = Math.abs(dynamicPercentage - goldenPercentageOutput);
-                int interval = 20;
-                int animat_duration = 3000;
-                float step = interval * goldenPercentageOutput / animat_duration;
-                while(dynamicPercentage <= goldenPercentageOutput && keepRunning) {
-                    Thread.sleep(interval);
-                    s1.setValue(s2.getValue().floatValue() * dynamicPercentage / (1 - dynamicPercentage));
-                    dynamicPercentage += step;
-                    updatePie();
-                    updateUIInUIThread();    // This has to be done in UI Thread!!
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        protected void updateUIInUIThread() {
-            stressScoreTextView.post(new Runnable() {
-                @Override
-                public void run() {
-                    updateDonutText();
-                    updateDonutColor();
-                    updateButtonsColor();
-                }
-            });
-        }
-    }
-
-    protected void redrawData() {
-        // @TODO: this is just demo data, will replace with realworld data later
-        Random rand = new Random();
-        goldenPercentageOutput = (90 + rand.nextInt(10)) / 100.f;
-        data.stopThread();
-        myThread = new Thread(data);
-        myThread.start();
-    }
-
-    protected void translateProgress2Duration(SeekBar seekBar) {
-        final int progress = seekBar.getProgress();
-        durationTextView.post(new Runnable() {
-            @Override
-            public void run() {
-                String base = "For the past one ";
-                switch (progress) {
-                    case 1:
-                        durationTextView.setText(base + "day");
-                        break;
-                    case 2:
-                        durationTextView.setText(base + "week");
-                        break;
-                    case 3:
-                        durationTextView.setText(base + "month");
-                        break;
-                    default:
-                        durationTextView.setText("For now");
-                }
+        button = (Button)findViewById(R.id.goto_last);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mPager.setCurrentItem(NUM_ITEMS-1);
             }
         });
     }
 
-    protected void updatePie() {
-        sf1.getFillPaint().setColor(getDynamicColor());
-        pie.redraw();
-    }
+    public static class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-    protected void updateDonutText() {
-        stressScoreTextView.setText(String.format("%.0f", dynamicPercentage * 100));
-    }
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
 
-    protected void updateDonutColor() {
-        stressScoreTextView.setTextColor(getDynamicColor());
-    }
-
-    protected void updateButtonsColor() {
-        for(int i : buttonids) {
-            Button b = (Button) findViewById(i);
-            b.setBackgroundColor(getDynamicColor());
+        @Override
+        public Fragment getItem(int position) {
+            return ArrayListFragment.newInstance(position);
         }
     }
 
-    protected int getDynamicColor() {
-        Color c = new Color();
-        return c.rgb(
-                Math.round(dynamicColorR * dynamicPercentage),
-                dynamicColorG,
-                Math.round(dynamicColorB)
-        );
-    }
+    public static class ArrayListFragment extends ListFragment {
+        int mNum;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        /**
+         * Create a new instance of CountingFragment, providing "num"
+         * as an argument.
+         */
+        static ArrayListFragment newInstance(int num) {
+            ArrayListFragment f = new ArrayListFragment();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            f.setArguments(args);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            return f;
         }
 
-        return super.onOptionsItemSelected(item);
+        /**
+         * When creating, retrieve this instance's number from its arguments.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+        }
+
+        /**
+         * The Fragment's UI is just a simple text view showing its
+         * instance number.
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_pager_list, container, false);
+            View tv = v.findViewById(R.id.text);
+            ((TextView)tv).setText("Fragment #" + mNum);
+            return v;
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            setListAdapter(new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, Cheeses.sCheeseStrings));
+        }
+
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            Log.i("FragmentList", "Item clicked: " + id);
+        }
     }
 }
+//END_INCLUDE(complete)
