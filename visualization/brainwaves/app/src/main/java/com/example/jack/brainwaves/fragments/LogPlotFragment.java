6be9fa7957c35d77e2d28f8a44ac48328e701d5d
Junjie Guan <gjj2684@gmail.com>
2015-03-06 14:07:41 -0500

    Enahcne 2

--- visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/fragments/LogPlotFragment.java
+++ visualization/brainwaves/app/src/main/java/com/example/jack/brainwaves/fragments/LogPlotFragment.java
@@ -195,10 +195,14 @@ public class LogPlotFragment extends SuperAwesomeCardFragment implements SeekBar
 
         ArrayList<Entry> yVals = new ArrayList<Entry>();
         for (int i = 0; i < count; i++) {
-            float val = (float) (Math.random() * range * 0.8 + 1);// + (float)
-            // ((mult *
-            // 0.1) / 10);
-            yVals.add(new Entry(val, i));
+            if(range == 0){
+                float val = (float) (Math.random() * range * 0.8 + 1);// + (float)
+                // ((mult *
+                // 0.1) / 10);
+                yVals.add(new Entry(val, i));
+            } else {
+                yVals.add(new Entry(0, i));
+            }
         }
 
         // create a dataset and give it a type
